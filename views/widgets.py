2b8340d17d206dcef04b00ec3c6730c215ee2f18
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-05 12:30:01 +1200

    Adjusted custom ItemTable widget

--- views/widgets.py
+++ views/widgets.py
@@ -1,6 +1,6 @@
-from customtkinter import *
+from customtkinter import CTkFrame, CTkEntry, CTkButton, CTkLabel, CTkScrollableFrame
 
-class EntryButton():
+class EntryButton:
     def __init__(self):
         self.frame = CTkFrame()
         self.frame.grid(row=0, column=0, sticky="nsew")
@@ -11,23 +11,20 @@ class EntryButton():
         self.button = CTkButton(self.frame, text="Enter")
         self.button.grid(row=0, column=1, sticky="nsew")
 
-class ItemTable():
-    def __init__(self):
-        self.frame = CTkFrame()
-        self.frame.grid(row=0, column=0, sticky="nsew")
-
-        self.header_frame = CTkFrame(self.frame)
-        self.header_frame.grid(row=0, column=0, sticky="nsew")
+class ItemTable(CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.grid(row=0, column=0, sticky="nsew")
 
         keys = "Item ID, Item name, On hand, On order, Available, Total value"
 
         columns = 0
         for i, key in enumerate(keys):
-            label = CTkLabel(self.header_frame, text=key)
+            label = CTkLabel(self, text=key)
             label.grid(row=0, column=i, sticky="nsew")
             columns += 1
 
-        self.scrollable_frame = CTkScrollableFrame(self.frame)
+        self.scrollable_frame = CTkScrollableFrame(self)
         self.scrollable_frame.grid(row=1, column=0, sticky="nsew")
 
         self.content_frame = CTkFrame(self.scrollable_frame)
