ffaa5891c0562880b1c78f7979c3de13aeb52252
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-05 12:31:04 +1200

    Changed variables to be in base_view

--- views/configure_view.py
+++ views/configure_view.py
@@ -1,27 +1,25 @@
 from customtkinter import CTkLabel, CTkButton
+from views.widgets import ItemTable
 from views.base_view import BaseView
 
 class ConfigureView(BaseView):
     def __init__(self, parent, controller):
         super().__init__(parent, controller)
 
-        base_font=("Roboto", 20)
-        base_padx=15
-        base_ipady=15
-
         label = CTkLabel(self.header_frame, text="Configure Items", font=("Roboto", 50))
-        label.grid(row=1, column=0, sticky="nsw", padx=base_padx)
+        label.grid(row=1, column=0, sticky="nsw", padx=self.base_padx)
 
-        save_button = CTkButton(self.header_frame, text="Save", font=base_font)
-        save_button.grid(row=1, column=1, padx=base_padx, ipady=base_ipady)
+        save_button = CTkButton(self.header_frame, text="Save", font=self.base_font)
+        save_button.grid(row=1, column=1, padx=self.base_padx, ipady=self.base_ipady)
 
-        import_button = CTkButton(self.header_frame, text="Import", font=base_font)
-        import_button.grid(row=1, column=2, padx=base_padx, ipady=base_ipady)
+        import_button = CTkButton(self.header_frame, text="Import", font=self.base_font)
+        import_button.grid(row=1, column=2, padx=self.base_padx, ipady=self.base_ipady)
 
-        add_button = CTkButton(self.control_frame, text="Add", font=base_font)
-        add_button.grid(row=1, column=0, padx=base_padx, ipady=base_ipady)
+        add_button = CTkButton(self.control_frame, text="Add", font=self.base_font)
+        add_button.grid(row=1, column=0, padx=self.base_padx, ipady=self.base_ipady)
 
-        remove_button = CTkButton(self.control_frame, text="Remove", font=base_font)
-        remove_button.grid(row=1, column=1, padx=base_padx, ipady=base_ipady)
+        remove_button = CTkButton(self.control_frame, text="Remove", font=self.base_font)
+        remove_button.grid(row=1, column=1, padx=self.base_padx, ipady=self.base_ipady)
 
-
+        item_table = ItemTable(self.content_frame)
+        item_table.grid(row=0, column=0, padx=self.base_padx, ipady=self.base_ipady)
