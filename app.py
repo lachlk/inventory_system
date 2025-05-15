112eca8d4160a0bfc50fe2b6686c1525c080def7
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-15 14:23:05 +1200

    Created function for changing pages

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from customtkinter import CTkFrame
+from views.stock_view import StockView
 from views.configure_view import ConfigureView
 
 class App:
     def __init__(self, root):
         self.root = root
-
-        self.container = CTkFrame(self.root)
+        self.frames = {}
+        self.container = CTkFrame(root)
         self.container.pack(fill="both", expand=True)
 
-        self.page = ConfigureView(self.container, self)
-        self.page.tkraise()
+        self.container.grid_rowconfigure(0, weight=1)
+        self.container.grid_columnconfigure(0, weight=1)
+
+        for F in (StockView, ConfigureView):
+            frame = F(self.container, self)
+            self.frames[F.__name__] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+            
+        self.show_frame("StockView")
+            
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
