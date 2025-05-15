be61a024401e1ee2401c12b0da77dccceb97e517
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-15 23:14:19 +1200

    Added function to save deleted data

--- views/configure_view.py
+++ views/configure_view.py
@@ -2,6 +2,8 @@ from customtkinter import CTkLabel, CTkButton
 from views.widgets import ItemTable
 from views.base_view import BaseView
 from controllers.data_manager import DataManager
+from views.widgets import AddPopUp
+
 
 class ConfigureView(BaseView):
     def __init__(self, parent, controller):
@@ -27,7 +29,7 @@ class ConfigureView(BaseView):
             self.header_frame,
             text="Save",
             font=self.base_font,
-            command=lambda: self.controller.show_frame("StockView"),
+            command=self.save,
         )
         self.save_button.grid(
             row=1,
@@ -53,7 +55,7 @@ class ConfigureView(BaseView):
             self.control_frame,
             text="Add",
             font=self.base_font,
-            state="disabled"
+            command=self.open_pop_up
         )
         self.add_button.grid(
             row=1,
@@ -62,20 +64,7 @@ class ConfigureView(BaseView):
             ipady=self.base_ipady,
         )
 
-        self.remove_button = CTkButton(
-            self.control_frame,
-            text="Remove",
-            font=self.base_font,
-            state="disabled",
-        )
-        self.remove_button.grid(
-            row=1,
-            column=1,
-            padx=self.base_padx,
-            ipady=self.base_ipady,
-        )
-
-        self.item_table = ItemTable(self.content_frame, configurable=True, data_manager=self.data_manager)
+        self.item_table = ItemTable(self.content_frame, configurable=True)
         self.item_table.grid(
             row=0,
             column=0,
@@ -83,3 +72,22 @@ class ConfigureView(BaseView):
             sticky="nsew",
             )
         self.item_table.populate(self.data_manager.items)
+
+    def save(self):
+        deleted_ids = self.item_table.get_deleted_items()
+        if deleted_ids:
+            self.data_manager.delete_item_by_id(deleted_ids)
+        self.item_table.deleted_items.clear()
+        self.controller.show_frame("StockView")
+        stock_view = self.controller.frames["StockView"]
+        stock_view.data_manager.items = stock_view.data_manager.load_items()
+        stock_view.item_table.populate(stock_view.data_manager.items)
+
+    def open_pop_up(self):
+        self.add_button.configure(state="disabled")
+        AddPopUp(
+            self,
+            self.data_manager,
+            self.item_table,
+            add_button=self.add_button,
+        )
