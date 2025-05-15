ffbb446c0fbb23a317470ddd70c1c2b3c65c2e7d
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-15 19:10:07 +1200

    Added item deletion

--- controllers/data_manager.py
+++ controllers/data_manager.py
@@ -1,11 +1,20 @@
 import json
 
+
 class DataManager:
     def __init__(self, filename):
+
         self.filename = filename
         self.items = self.load_items()
 
     def load_items(self):
+
         with open(self.filename, 'r') as file:
             return json.load(file)
-        
+
+    def delete_item_by_id(self, item_id):
+        self.items = [item for item in self.items if item["Item ID"] != item_id]
+        with open(self.filename, "w") as f:
+            json.dump(self.items, f, indent=4)
+        print(f"Deleted item with id {item_id}")
+        self.load_items()
--- views/widgets.py
+++ views/widgets.py
@@ -1,4 +1,6 @@
 from customtkinter import CTkFrame, CTkEntry, CTkButton, CTkLabel, CTkScrollableFrame
+from controllers.data_manager import DataManager
+
 
 class EntryButton:
     def __init__(self):
@@ -11,9 +13,12 @@ class EntryButton:
         self.button = CTkButton(self.frame, text="Enter")
         self.button.grid(row=0, column=1, sticky="nsew")
 
+
 class ItemTable(CTkFrame):
-    def __init__(self, parent):
+    def __init__(self, parent, configurable, data_manager):
         super().__init__(parent)
+        self.configurable = configurable
+        self.data_manager = data_manager
 
         self.item_keys = ["Item ID", "Item name", "On hand", "On order", "Available", "Total value"]
 
@@ -33,9 +38,28 @@ class ItemTable(CTkFrame):
             label.grid(row=0, column=i, sticky="nsew")
 
     def populate(self, items):
+        for widget in self.scrollable_frame.winfo_children():
+            widget.destroy()
+
         for i, item in enumerate(items):
+            row_labels = []
+            item_id = item.get("Item ID")
+
             for j, key in enumerate(item):
                 self.scrollable_frame.grid_columnconfigure(j, uniform="equal", weight=1)
                 value = item.get(key, "")
                 label = CTkLabel(self.scrollable_frame, text=value, corner_radius=10)
+
+                if self.configurable == True:
+                    label.bind("<Enter>", lambda e, label=label: label.configure(fg_color="red"))
+                    label.bind("<Leave>", lambda e, label=label: label.configure(fg_color="transparent"))
+
                 label.grid(row=i, column=j, sticky="nsew")
+                row_labels.append(label)
+                
+            if self.configurable == True:
+                for label in row_labels:
+                    label.bind("<Button-1>", lambda e, item_id=item_id, labels=row_labels: (
+                        self.data_manager.delete_item_by_id(item_id),
+                        [lbl.destroy() for lbl in labels]
+                    ))
