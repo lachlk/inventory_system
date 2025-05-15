4fd1ad5c6716da04b5a59fb1442546ee29f01418
Lachlan <21lkidd@wakatipu.school.nz>
2025-05-15 23:13:20 +1200

    Id generator in DataManager

--- controllers/data_manager.py
+++ controllers/data_manager.py
@@ -12,9 +12,21 @@ class DataManager:
         with open(self.filename, 'r') as file:
             return json.load(file)
 
-    def delete_item_by_id(self, item_id):
-        self.items = [item for item in self.items if item["Item ID"] != item_id]
+    def delete_item_by_id(self, item_ids):
+        self.items = [item for item in self.items if item["Item ID"] not in item_ids]
         with open(self.filename, "w") as f:
             json.dump(self.items, f, indent=4)
-        print(f"Deleted item with id {item_id}")
-        self.load_items()
+
+    def generate_new_id(self):
+        existing_ids = [item["Item ID"] for item in self.items]
+        numbers = []
+
+        for item_id in existing_ids:
+            try:
+                num = int(item_id)
+                numbers.append(num)
+            except (IndexError, ValueError):
+                continue
+
+        next_number = max(numbers, default=0) + 1
+        return str(next_number)
